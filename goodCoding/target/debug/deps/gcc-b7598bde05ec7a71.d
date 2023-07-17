959bb683f6f748a0f97f6868e426c2ca68be972e
nownver <urawee.thani@gmail.com>
2023-07-17 19:34:27 +0700

    fix

--- goodCoding/target/debug/deps/gcc-b7598bde05ec7a71.d
+++
@@ -1,8 +0,0 @@
-/Users/yokurawee/rust_work/goodCoding/target/debug/deps/gcc-b7598bde05ec7a71.rmeta: /Users/yokurawee/.cargo/registry/src/github.com-1ecc6299db9ec823/gcc-0.3.55/src/lib.rs /Users/yokurawee/.cargo/registry/src/github.com-1ecc6299db9ec823/gcc-0.3.55/src/windows_registry.rs
-
-/Users/yokurawee/rust_work/goodCoding/target/debug/deps/libgcc-b7598bde05ec7a71.rlib: /Users/yokurawee/.cargo/registry/src/github.com-1ecc6299db9ec823/gcc-0.3.55/src/lib.rs /Users/yokurawee/.cargo/registry/src/github.com-1ecc6299db9ec823/gcc-0.3.55/src/windows_registry.rs
-
-/Users/yokurawee/rust_work/goodCoding/target/debug/deps/gcc-b7598bde05ec7a71.d: /Users/yokurawee/.cargo/registry/src/github.com-1ecc6299db9ec823/gcc-0.3.55/src/lib.rs /Users/yokurawee/.cargo/registry/src/github.com-1ecc6299db9ec823/gcc-0.3.55/src/windows_registry.rs
-
-/Users/yokurawee/.cargo/registry/src/github.com-1ecc6299db9ec823/gcc-0.3.55/src/lib.rs:
-/Users/yokurawee/.cargo/registry/src/github.com-1ecc6299db9ec823/gcc-0.3.55/src/windows_registry.rs:
--- goodCoding/target/debug/goodCoding.d
+++
@@ -1,1 +0,0 @@
-/Users/yokurawee/rust_work/goodCoding/target/debug/goodCoding: /Users/yokurawee/rust_work/goodCoding/src/main.rs
